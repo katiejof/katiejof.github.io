71a1e171ca727599ce845e1682ecd2fa7703750a
Kathleen Cuccia-Fenton <kcuccia-fenton@MacBook-Air-6.local>
2018-07-24 16:08:50 -0400

    first commit

---
+++ style.js
@@ -0,0 +1,12 @@
+function myFunction() {
+}
+
+function myFunction1() {
+   document.getElementById("button1").style.background = "url('volleyball.jpg')";
+}
+function myFunction2() {
+  document.getElementById("button1").innerHTML = "Sports";
+  document.getElementById("button1").style.background = 0;
+  document.getElementById("button1").style.color='red';
+
+}
