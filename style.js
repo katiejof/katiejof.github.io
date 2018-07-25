7f463fc4eae357ed1339503a2572d15f9d7f1419
Kathleen Cuccia-Fenton <kcuccia-fenton@MacBook-Air-6.local>
2018-07-25 16:00:51 -0400

    new info

--- style.js
+++ style.js
@@ -1,12 +1,19 @@
-function myFunction() {
+/*function myFunction() {
+    document.getElementById("button1").style.background  = <url('volleyball.jpg')>;
+}
+*/
+function myFunction1() {
+  document.getElementById("button1").innerHTML= "<img src='volleyball.jpg'>";
+  // document.getElementById("button1").style.background = transparent;
+//document.getElementById("button1").style.visibility= hidden;
+//document.getElementById("button1").style.display= none;
+
 }
 
-function myFunction1() {
-   document.getElementById("button1").style.background = "url('volleyball.jpg')";
-}
 function myFunction2() {
   document.getElementById("button1").innerHTML = "Sports";
-  document.getElementById("button1").style.background = 0;
   document.getElementById("button1").style.color='red';
+  //document.getElementById("button1").style.visibility= visible;
+  //document.getElementById("button1").style.display= all;
 
 }
